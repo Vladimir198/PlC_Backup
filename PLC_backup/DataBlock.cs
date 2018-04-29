aa05e00784217ce286e1ff4225856bda64321c53
Владимир <rysev@a-vektor.ru>
2018-04-29 20:53:00 +0300

    Первый
    Signed-off-by: Владимир <rysev@a-vektor.ru>

---
+++ PLC_backup/DataBlock.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace PLC_backup
+{
+    [DataContract]
+    class DataBlock
+    {
+        [DataMember]
+        public String Name { get; set; }
+        [DataMember]
+        public int Number { get; set; }
+        [DataMember]
+        public int Size { get; set; }
+
+        public byte[] Data { get; set; }
+
+        public DataBlock()
+        {
+            
+        }
+
+        public DataBlock(string name, int number, int size)
+        {
+            Name = name;
+            Number = number;
+            Size = size;
+            Data = new byte[size];
+        }
+    }
+}
